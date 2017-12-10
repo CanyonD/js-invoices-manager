d4374c5fd650f3a365a51f262e6a1e1f989e58ec
Canyon Di <CanyonD8@gmail.com>
2017-12-11 01:42:02 +0200

    Fixed empty invoices.

--- src/Invoices.js
+++ src/Invoices.js
@@ -57,6 +57,10 @@ class Invoices extends Component {
   componentDidMount() {
     axios.get("http://localhost:8800/api/invoices").then(results => {
       let res = results.data;
+      if (res.length === 0)
+        this.setState({
+          invoices: []
+        });
       results.data.map((x, y) => {
         if (x.customer_id !== 0 && x.customer_id !== null) {
           res[y].customer = this.state.customers.find(
@@ -111,17 +115,15 @@ class Invoices extends Component {
             </div>
           </div>
         </div>
-        {this.state.invoices.length === 0 ? (
-          <Invoice {...this.props} />
-        ) : (
-          <Grid
-            {...this.props}
-            header={model}
-            data={this.state.invoices}
-            handleEdit={this.handleEdit}
-            handleRemove={this.handleRemove}
-          />
-        )}
+        {this.state.invoices.length === 0
+          ? <Invoice {...this.props} />
+          : <Grid
+              {...this.props}
+              header={model}
+              data={this.state.invoices}
+              handleEdit={this.handleEdit}
+              handleRemove={this.handleRemove}
+            />}
       </div>
     );
   }
